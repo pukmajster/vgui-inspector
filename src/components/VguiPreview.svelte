08f90687a21c743adc0de2146a13a71218dc8f8e
pukmajster <45610204+pukmajster@users.noreply.github.com>
2021-06-25 13:53:21 +0200

    Update layout

--- src/components/VguiPreview.svelte
+++ src/components/VguiPreview.svelte
@@ -1,11 +1,41 @@
 <script lang="ts">
+import { vguiResource } from "../stores/VguiStore";
+
+import VguiPanel from "./VguiPanel.svelte";
+
 	
 </script>
 
-<main>
-	<h1>Hello!</h1>
-</main>
+<div class="container">
+	<div class="vguiRoot">
+		{#if $vguiResource} 
+			{#each $vguiResource.children as child} 
+				<VguiPanel panel={child} />
+			{/each}
+		{/if}
+	</div>
+</div>
 
 <style lang="scss" >
-	
+	.container {
+		width: 100%;
+		height: 100%;
+
+		padding: 3em;
+
+		display: grid;
+		place-items: center;
+	}
+
+	.vguiRoot {
+		width: 640px;
+		height: 480px;
+
+		/* width: 100%; */
+		/* aspect-ratio: 16 / 9; */
+
+		background-color: rgb(28, 28, 28);
+
+		position: relative;
+	}
 </style>
