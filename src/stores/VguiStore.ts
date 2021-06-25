3f6e932959199ef166279014631ffafe923e8062
pukmajster <45610204+pukmajster@users.noreply.github.com>
2021-06-25 03:29:48 +0200

    Add currentEditing writable

--- src/stores/VguiStore.ts
+++ src/stores/VguiStore.ts
@@ -2,3 +2,4 @@ import { writable } from "svelte/store";
 import type { VguiPanel } from "../utils/VguiTypes";
 
 export const vguiResource = writable<VguiPanel | null>(null);
+export const currentEditingVguiPanel = writable<VguiPanel | null>(null);
