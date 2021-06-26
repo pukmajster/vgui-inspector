d59ce92131a42eb9f614ae578860cce402c7a2cc
pukmajster <45610204+pukmajster@users.noreply.github.com>
2021-06-26 04:13:07 +0200

    Update VguiChoices.ts

--- src/utils/VguiChoices.ts
+++ src/utils/VguiChoices.ts
@@ -4,6 +4,17 @@ export const vguiPinCorners = [
   'BOTTOMLEFT',
   'BOTTOMRIGHT'
 ]
+export const vguiPinCorners2 = [
+  'PIN_TOPLEFT',
+	'PIN_TOPRIGHT',
+	'PIN_BOTTOMLEFT',
+	'PIN_BOTTOMRIGHT',
+
+	'PIN_CENTER_TOP',
+	'PIN_CENTER_RIGHT',
+	'PIN_CENTER_BOTTOM',
+	'PIN_CENTER_LEFT',
+]
 
 export const vguiAutoResizeOptions = [
   'NO',
